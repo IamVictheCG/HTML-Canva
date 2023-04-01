3b83cb079ce15fc1b54842310b26299468bd57ff
Victor Okechukwu <VicOke360@gmail.com>
2023-04-01 19:20:43 +0100

    updates made

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 var canvas = document.getElementById('draw')
+var check = document.querySelector('.check')
 var context = canvas.getContext('2d')
 canvas.width = window.innerWidth
+context.strokeStyle = "#BADA55"
+context.lineCap = 'round'
+context.lineJoin= 'round'
+
+let isDrawing = false
+// let lastX = 0
+// let lastY = 0
+
+function draw(e) {
+    // console.log(e)
+    return e
+}
+
+check.innerHTML = canvas.addEventListener('mousemove', draw())
+
