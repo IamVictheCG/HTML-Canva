bc9dcb416c6b9afbc28bd267fd7ebe15972f8e3e
Victor Okechukwu <VicOke360@gmail.com>
2023-04-02 01:10:49 +0100

    updates made

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
 var canvas = document.getElementById('draw')
 var check = document.querySelector('.check')
 var context = canvas.getContext('2d')
-canvas.width = window.innerWidth
-context.strokeStyle = "#BADA55"
-context.lineCap = 'round'
-context.lineJoin= 'round'
+canvas.width = window.innerWidth;
+context.strokeStyle = `BADA55`;
+context.lineCap = 'round';
+context.lineJoin= 'round';
+context.lineWidth = 40;
 
 let isDrawing = false
 let lastX = 0; 
 let lastY = 0; 
+let hue = 0
 
 function draw(e) {
     if (!isDrawing) return
     console.log(e)
-
+    
     context.beginPath();
     //starts from
     context.moveTo(lastX, lastY);
     //goes to
     context.lineTo(e.offsetX, e.offsetY);
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     context.stroke();
+    // [lastX, lastY] = [e.offsetX, e.offsetY];
+    lastY = e.offsetY;
+    lastX = e.offsetX;
+    hue++;
+
+    if(hue >= 360) {
+        hue = 0
+    }
 }
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     lastY = e.offsetY;
     lastX = e.offsetX;
-    // [lastX, lastY] = [e.offsetX, e.offsetY];
 })
 canvas.addEventListener('mousemove', draw)
 canvas.addEventListener('mouseup', () => isDrawing = false)
